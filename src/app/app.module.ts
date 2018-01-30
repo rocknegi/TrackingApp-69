76cd142d5e160fbf8a7057ef92044dbc7fe1e270
rocknegi <rocknegi53@gmail.com>
2018-01-31 00:40:20 +0530

    initial commit :3

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutes } from "./app.routes";
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { AuthService } from './services/auth.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutes,
+    AngularFireModule.initializeApp(environment.firebase, 'angular-auth-firebase'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+ export class AppModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthService {
+  private user: Observable<firebase.User>;
+  private userDetails: firebase.User = null;
+
+constructor(private firebaseAuth: AngularFireAuth, private router: Router) { 
+      this.user = firebaseAuth.authState;
+      this.user.subscribe(
+        (user) => {
+          if (user) {
+            this.userDetails = user;
+            console.log(this.userDetails);
+          }
+          else {
+            this.userDetails = null;
+          }
+        }
+      )
+  }
+
+  signInWithTwitter() {
+    return this.firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.TwitterAuthProvider()
+    )
+  }
+
+  signInWithFacebook() {
+    return this.firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.FacebookAuthProvider()
+    )
+  }
+
+  signInWithGoogle() {
+    return this.firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    )
+  }
+
+  signInWithGithub() {
+    return this.firebaseAuth.auth.signInWithPopup(
+        new firebase.auth.GithubAuthProvider()
+    )
+}
+
+}
+
+
+  
