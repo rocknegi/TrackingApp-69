dfb8effca5bf93efddcad1ac65d4996a1a1f96f5
rocknegi <rocknegi53@gmail.com>
2018-02-04 15:17:16 +0530

    maybe finally fixed heroku problem  address this issue : Property 'firebase' does not exist on type '{ production: boolean; }'.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-//Install express server
 const express = require('express');
 const app = express();
-
-// Serve only the static files form the dist directory
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firebaseAuth: AngularFireAuth, private router: Router) { }
+
+
+  logout() {
+    this.firebaseAuth.auth.signOut()
+    .then((res) => this.router.navigate(['/']));
+  }
+
 
   ngOnInit() {
   }
