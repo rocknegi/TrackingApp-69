48e6c5794685d78fd84bb00dff1d7576f837255d
rocknegi <rocknegi53@gmail.com>
2018-02-10 13:01:11 +0530

    track user location

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -13,9 +13,10 @@ export class DashboardComponent implements OnInit {
   private user: Observable<firebase.User>;
   private userDetails: firebase.User = null;
   
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number
+  lng: number
   showMap = false;
+  geolocationPosition :any
 
   constructor(private firebaseAuth: AngularFireAuth, private router: Router) {
     this.user = firebaseAuth.authState;
@@ -39,12 +40,36 @@ export class DashboardComponent implements OnInit {
   }
 
   map(){
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+          position => {
+              this.geolocationPosition = position,
+                  console.log(position)
+                  this.lat=position.coords.latitude
+                  this.lng=position.coords.longitude
+          },
+          error => {
+              switch (error.code) {
+                  case 1:
+                      console.log('Permission Denied');
+                      break;
+                  case 2:
+                      console.log('Position Unavailable');
+                      break;
+                  case 3:
+                      console.log('Timeout');
+                      break;
+              }
+          }
+      );
+  };
     this.showMap = true;
     console.log(this.showMap)
   }
 
 
   ngOnInit() {
+
   }
 
 }
